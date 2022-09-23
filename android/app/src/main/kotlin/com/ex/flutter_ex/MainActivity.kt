49d9abb94d1f096a75e517f301fe56f36711e5c1
steven <steven220705@gmail.com>
2022-09-23 16:59:59 +0700

    init

---
+++ android/app/src/main/kotlin/com/ex/flutter_ex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ex.flutter_ex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('hello world'),
+        centerTitle: true,
+        leading: const Icon(Icons.abc),
+      ),
+      body: Center(child: Obx(() => Text("Home Page ${controller.count}"))),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(height: 50.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => controller.increment(),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
